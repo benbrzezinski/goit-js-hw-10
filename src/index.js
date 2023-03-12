beb2e5493293ed4fe657aab933d3c979f233fe02
Beniamin Brzeziński <benek9311@wp.pl>
2023-03-12 19:52:38 +0100

    Task completed

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
 const searchBox = document.querySelector('input#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+const notifyOptions = {
+  position: 'center-top',
+  distance: '20px',
+  timeout: 3500,
+  pauseOnHover: false,
+};
 
 const getCountry = debounce(() => {
   const trimmedSearchBoxValue = searchBox.value.trim();
 
   if (!trimmedSearchBoxValue) {
-    countryList.innerHTML = '';
-    return;
+    return erase();
   }
 
   fetchCountries()
@@ -22,11 +27,28 @@ const getCountry = debounce(() => {
         common.toLowerCase().startsWith(trimmedSearchBoxValue.toLowerCase())
       );
 
-      if (filteredCountries.length === 0) {
-        countryList.innerHTML = '';
-        return;
+      if (!filteredCountries.length) {
+        erase();
+        return Notify.failure(
+          'Oops, there is no country with that name',
+          notifyOptions
+        );
       }
 
+      if (filteredCountries.length > 10) {
+        erase();
+        return Notify.info(
+          'Too many matches found. Please enter a more specific name.',
+          notifyOptions
+        );
+      }
+
+      if (filteredCountries.length === 1) {
+        erase();
+        return renderCountryInfo(filteredCountries);
+      }
+
+      erase();
       renderCountryList(filteredCountries);
     })
     .catch(error => console.error(error));
@@ -35,16 +57,44 @@ const getCountry = debounce(() => {
 searchBox.addEventListener('input', getCountry);
 
 function renderCountryList(filteredCountries) {
-  console.log(filteredCountries);
-  countryList.innerHTML = '';
-
   const countryTags = filteredCountries
     .map(({ name: { common }, flags: { svg } }) => {
       return `<li class="country-item">
-      <img class="country-img" src="${svg}" width="40px" height="25px" />
-      <p class="country-name">${common}</p></li>`;
+              <img class="country-img" src="${svg}" width="40px" height="25px" />
+              <p class="country-name">${common}</p></li>`;
     })
     .join('');
 
-  countryList.insertAdjacentHTML('beforeend', countryTags);
+  countryList.insertAdjacentHTML('afterbegin', countryTags);
+}
+
+function renderCountryInfo(filteredCountries) {
+  const countryTags = filteredCountries
+    .map(
+      ({
+        name: { common },
+        flags: { svg },
+        capital,
+        population,
+        languages,
+      }) => {
+        const languagesArray = Object.values(languages);
+
+        return `<div class="country-info-box">
+                <img class="country-img" src="${svg}" width="50px" height="35px" />
+                <h3 class="country-info-name">${common}</h3></div>
+                <p class="country-info-text"><span class="country-info-text-first-word">Capital:</span> ${capital}</p>
+                <p class="country-info-text"><span class="country-info-text-first-word">Population:</span> ${population}</p>
+                <p class="country-info-text"><span class="country-info-text-first-word">Languages:
+                </span> ${languagesArray.join(', ')}</p>`;
+      }
+    )
+    .join('');
+
+  countryInfo.insertAdjacentHTML('afterbegin', countryTags);
+}
+
+function erase() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
 }
